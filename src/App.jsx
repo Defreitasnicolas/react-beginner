4648814ec76589c1758a640e896f1085f8596191
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-10-17 00:01:46 +0200

    ajout de filtres

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,38 @@ import Card from "./component/card";
 import { useState } from "react";
 
 const arrayTravel = [
-  { destination: "Maldives", img: "src/assets/maldives.jpg" },
-  { destination: "Cap Vert", img: "src/assets/cap-vert.jpg" },
-  { destination: "Brugge", img: "src/assets/brugge.jpg" },
-  { destination: "Guadeloupe", img: "src/assets/guadeloupe.jpg" },
+  { destination: "Maldives", img: "src/assets/maldives.jpg", style: "Soleil" },
+  { destination: "Cap Vert", img: "src/assets/cap-vert.jpg", style: "Soleil" },
+  { destination: "Brugge", img: "src/assets/brugge.jpg", style: "Villes" },
+  {
+    destination: "Guadeloupe",
+    img: "src/assets/guadeloupe.jpg",
+    style: "Soleil",
+  },
+  { destination: "Annecy", img: "src/assets/annecy.jpg", style: "Montagne" },
+  {
+    destination: "Groenland",
+    img: "src/assets/groenland.jpg",
+    style: "Montagne",
+  },
+  { destination: "NewYork", img: "src/assets/newyork.jpg", style: "Villes" },
+  {
+    destination: "Polynesie",
+    img: "src/assets/polynesie.jpg",
+    style: "Soleil",
+  },
+  { destination: "Rio", img: "src/assets/rio.jpg", style: "Soleil" },
+  {
+    destination: "St-Petersburg",
+    img: "src/assets/st-petersburg.jpg",
+    style: "Villes",
+  },
+  { destination: "Zanzibar", img: "src/assets/zanzibar.jpg", style: "Soleil" },
 ];
-
-
-
 
 function App() {
   const [index, setIndex] = useState(0);
-  
+
   return (
     <>
       <div>
@@ -23,7 +43,7 @@ function App() {
           destination={arrayTravel[index].destination}
           index={index}
           setIndex={setIndex}
-          arrayTravel2={arrayTravel}
+          arrayTravel={arrayTravel}
         />
       </div>
     </>
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import "./card.css";
+import { useState } from "react";
 
-
-const Card = ({ destination, picture, index, setIndex, arrayTravel2 }) => {
+const Card = ({ destination, picture, index, setIndex, arrayTravel }) => {
+  const [filteredData, setFilteredData] = useState(arrayTravel);
+  console.log(index);
+  const handleClick = (style) => {
+    setFilteredData(arrayTravel.filter((element) => element.style === style));
+  };
   
   return (
     <>
+      <div className="filter">
+        <button type="button" onClick={() => handleClick("Soleil")}>
+          Soleil
+        </button>
+        <button type="button" onClick={() => handleClick("Montagne")}>
+          Montagne
+        </button>
+        <button type="button" onClick={() => handleClick("Villes")}>
+          Villes
+        </button>
+        <button type="button" onClick={() => (setFilteredData(arrayTravel) , setIndex(0))}>Reset</button>
+      </div>
+      <h2>Envie de voyager?</h2>
       <div className="card">
         <div className="img">
-          <img src={picture} alt="photo de destination" />
+          <img src={filteredData[index].img} alt="photo de destination" />
         </div>
-        <p>{destination}</p>
+        <p>{filteredData[index].destination}</p>
         <div className="button">
-          {(index > 0) ? (<button onClick={() => setIndex(index - 1)}>precedent</button>):("")}
-          {(index < arrayTravel2.length - 1) ? (<button onClick={() => setIndex(index + 1)}>suivant</button>):("")}
+          {index > 0 ? (
+            <button onClick={() => setIndex(index - 1)}>Précédent</button>
+          ) : (
+            ""
+          )}
+          {index < filteredData.length - 1 ? (
+            <button onClick={() => setIndex(index + 1)}>Suivant</button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </>
